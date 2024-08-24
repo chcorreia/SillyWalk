3321ad399de3ae91c5af592841e2307559cfcc71
chcorreia <chcorreia@gmail.com>
2024-08-24 02:26:56 -0300

    slate zero

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+Module name: SillyWalk
+
+Description:
+    Animates a robot walking through a board in a jupyter notebook
+    as a tool for learning introductory programming
+
+Author:
+    Carlos H Correia (chcorreia@gmail.com)
+
+Source:
+    github.com/chcorreia/SillyWalk
+
+Version:
+    0.1.0
+
+Usage:
+    import the translation using 'from SillyWalk import LOCALE'
+    where LOCALE is the ISO code for your language
+"""
